edfb7c850cc97736f63b23fc7cacc6218cc38b23
Manjunath <bhandarimanjunath98@gmail.com>
2025-11-10 15:51:06 +0530

    Fix datasource url

--- src/main/java/com/quodex/dineboard/DineboardApplication.java
+++ src/main/java/com/quodex/dineboard/DineboardApplication.java
@@ -7,7 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DineboardApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DineboardApplication.class, args);
-	}
+
+        SpringApplication.run(DineboardApplication.class, args);
+        System.out.println(">>> DATABASE_URL = " + System.getenv("DATABASE_URL"));
+        System.out.println(">>> DATASOURCE_URL = " + System.getenv("DATASOURCE_URL"));
+
+    }
 
 }
